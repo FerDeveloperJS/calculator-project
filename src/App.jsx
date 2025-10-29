3d8bc8637fa809cf613c3afade579e4ceb0ea411
FerDeveloperJS <ferchosoftwaredev@gmail.com>
2025-10-29 18:01:21 -0500

    Added dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,52 @@
 import { useState } from "react";
 import Calculator from "./components/calculator";
+import CalculatorDarkMode from "./components/CalculatorDarkMode";
+import MoonSvg from "./assets/svg/MoonSvg";
 
 function App() {
   const [screen, setScreen] = useState("");
+  const [darkMode, setDarkMode] = useState(false);
   const [currentOperation, setCurrentOperation] = useState(0);
   return (
-    <div className="flex flex-col gap-16 justify-center items-center min-h-screen">
-      <h1 className="font-bold text-3xl md:text-5xl mb-10">
-        Calculator APP By Fer
-      </h1>
-      <div className="flex justify-center items-center border border-black h-16 w-60 md:w-80 md:h-20 p-3.5 rounded-2xl text-xl">
-        {screen}
-      </div>
-      <Calculator
-        setCurrentOperation={setCurrentOperation}
-        screen={screen}
-        setScreen={setScreen}
-      />
-    </div>
+    <>
+      {darkMode ? (
+        <div className="bg-[#121212] flex flex-col gap-16 justify-center items-center min-h-screen">
+          <MoonSvg setDarkMode={setDarkMode} darkMode={darkMode} />
+
+          <h1 className="text-[#F5F5F5] font-bold text-3xl md:text-5xl mb-10">
+            Calculator APP By Fer
+          </h1>
+
+          <div className="bg-[#2A2A2A] text-[#F5F5F5] flex justify-center items-center border border-black h-16 w-60 md:w-80 md:h-20 p-3.5 rounded-2xl text-xl">
+            {screen}
+          </div>
+
+          <CalculatorDarkMode
+            setCurrentOperation={setCurrentOperation}
+            screen={screen}
+            setScreen={setScreen}
+          />
+        </div>
+      ) : (
+        <div className="flex flex-col gap-16 justify-center items-center min-h-screen">
+          <MoonSvg setDarkMode={setDarkMode} darkMode={darkMode} />
+
+          <h1 className="font-bold text-3xl md:text-5xl mb-10">
+            Calculator APP By Fer
+          </h1>
+
+          <div className="flex justify-center items-center border border-black h-16 w-60 md:w-80 md:h-20 p-3.5 rounded-2xl text-xl">
+            {screen}
+          </div>
+
+          <Calculator
+            setCurrentOperation={setCurrentOperation}
+            screen={screen}
+            setScreen={setScreen}
+          />
+        </div>
+      )}
+    </>
   );
 }
 
---
+++ src/components/CalculatorDarkMode.jsx
@@ -0,0 +1,135 @@
+function CalculatorDarkMode({
+  screen,
+  setScreen,
+  currentOperation,
+  setCurrentOperation,
+}) {
+  return (
+    <div className="relative">
+      <div className="flex flex-wrap w-60 md:w-80 text-white font-bold gap-2.5">
+        <button
+          onClick={() => setScreen(screen + "7")}
+          className="flex justify-center items-center bg-[#333333] w-14 h-14  md:w-20 md:h-20 p-1.5 text-2xl rounded-full"
+        >
+          7
+        </button>
+
+        <button
+          onClick={() => setScreen(screen + "8")}
+          className="flex justify-center items-center bg-[#333333] w-14 h-14 md:w-20 md:h-20  p-1.5 text-2xl rounded-full"
+        >
+          8
+        </button>
+
+        <button
+          onClick={() => setScreen(screen + "9")}
+          className="flex justify-center items-center bg-[#333333] w-14 h-14 md:w-20 md:h-20  p-1.5 text-2xl rounded-full"
+        >
+          9
+        </button>
+
+        <button
+          onClick={() => setScreen(screen + "4")}
+          className="flex justify-center items-center bg-[#333333] w-14 h-14 md:w-20 md:h-20  p-1.5 text-2xl rounded-full"
+        >
+          4
+        </button>
+
+        <button
+          onClick={() => setScreen(screen + "5")}
+          className="flex justify-center items-center bg-[#333333] w-14 h-14 md:w-20 md:h-20  p-1.5 text-2xl rounded-full"
+        >
+          5
+        </button>
+
+        <button
+          onClick={() => setScreen(screen + "6")}
+          className="flex justify-center items-center bg-[#333333] w-14 h-14 md:w-20 md:h-20  p-1.5 text-2xl rounded-full"
+        >
+          6
+        </button>
+
+        <button
+          onClick={() => setScreen(screen + "1")}
+          className="flex justify-center items-center bg-[#333333] w-14 h-14 md:w-20 md:h-20  p-1.5 text-2xl rounded-full"
+        >
+          1
+        </button>
+
+        <button
+          onClick={() => setScreen(screen + "2")}
+          className="flex justify-center items-center bg-[#333333] w-14 h-14 md:w-20 md:h-20  p-1.5 text-2xl rounded-full"
+        >
+          2
+        </button>
+
+        <button
+          onClick={() => setScreen(screen + "3")}
+          className="flex justify-center items-center bg-[#333333] w-14 h-14 md:w-20 md:h-20  p-1.5 text-2xl rounded-full"
+        >
+          3
+        </button>
+      </div>
+
+      <div className="mt-2.5 flex gap-2.5 text-white">
+        <button
+          onClick={() => setScreen(screen + "+")}
+          className="flex justify-center items-center rounded-full p-1.5 w-14 h-14 md:w-20 md:h-20  text-4xl font-extrabold bg-[#A26BFF]"
+        >
+          +
+        </button>
+
+        <button
+          onClick={() => setScreen(screen + "-")}
+          className="flex justify-center items-center rounded-full p-1.5 w-14 h-14 md:w-20 md:h-20  text-4xl font-extrabold bg-[#A26BFF]"
+        >
+          -
+        </button>
+
+        <button
+          onClick={() => setScreen(screen + "*")}
+          className="flex justify-center items-center rounded-full p-1.5 w-14 h-14 md:w-20 md:h-20  text-4xl font-extrabold bg-[#A26BFF]"
+        >
+          x
+        </button>
+
+        <button
+          onClick={() => setScreen(screen + "/")}
+          className="flex justify-center items-center rounded-full p-1.5 w-14 h-14 md:w-20 md:h-20  text-4xl font-extrabold bg-[#A26BFF]"
+        >
+          ÷
+        </button>
+      </div>
+
+      <div className="absolute flex flex-col gap-2.5 top-0 right-0 h-64 rounded-full text-white font-bold">
+        <button
+          onClick={() => {
+            const result = eval(screen);
+            setCurrentOperation(result);
+            setScreen(result.toString());
+          }}
+          className="text-4xl w-14 h-14 md:w-20 md:h-20  p-1.5 rounded-full bg-[#A26BFF]"
+        >
+          {" "}
+          ={" "}
+        </button>
+
+        <button
+          onClick={() => setScreen(screen + ".")}
+          className="text-4xl w-14 h-14 md:w-20 md:h-20  p-1.5 rounded-full bg-[#333333]"
+        >
+          ,
+        </button>
+
+        <button
+          onClick={() => setScreen(screen + "0")}
+          className="flex justify-center items-center bg-[#333333] w-14 h-14 md:w-20 md:h-20  p-1.5 text-2xl rounded-full"
+        >
+          0
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default CalculatorDarkMode;
