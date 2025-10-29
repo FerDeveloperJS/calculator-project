7f22033d7b08cda1367281bb2891625a1ed9df5a
FerDeveloperJS <ferchosoftwaredev@gmail.com>
2025-10-29 17:18:17 -0500

    Added basic operations: sum, substraction, multiplication, division

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
+import { useState } from "react";
 import Calculator from "./components/calculator";
 
 function App() {
+  const [screen, setScreen] = useState("");
+  const [currentOperation, setCurrentOperation] = useState(0);
   return (
     <div className="flex flex-col gap-16 justify-center items-center min-h-screen">
       <h1 className="font-bold text-3xl md:text-5xl mb-10">
         Calculator APP By Fer
       </h1>
-      <div className="border border-black h-16 w-60 md:w-80 md:h-20 p-3.5 rounded-2xl"></div>
-      <Calculator />
+      <div className="flex justify-center items-center border border-black h-16 w-60 md:w-80 md:h-20 p-3.5 rounded-2xl text-xl">
+        {screen}
+      </div>
+      <Calculator
+        setCurrentOperation={setCurrentOperation}
+        screen={screen}
+        setScreen={setScreen}
+      />
     </div>
   );
 }
--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,78 +1,130 @@
-function Calculator() {
+function Calculator({
+  screen,
+  setScreen,
+  currentOperation,
+  setCurrentOperation,
+}) {
   return (
     <div className="relative">
       <div className="flex flex-wrap w-60 md:w-80 text-white font-bold gap-2.5">
-        <button className="flex justify-center items-center bg-[#4B4B4B] w-14 h-14  md:w-20 md:h-20 p-1.5 text-2xl rounded-full">
+        <button
+          onClick={() => setScreen(screen + "7")}
+          className="flex justify-center items-center bg-[#4B4B4B] w-14 h-14  md:w-20 md:h-20 p-1.5 text-2xl rounded-full"
+        >
           7
         </button>
 
-        <button className="flex justify-center items-center bg-[#4B4B4B] w-14 h-14 md:w-20 md:h-20  p-1.5 text-2xl rounded-full">
+        <button
+          onClick={() => setScreen(screen + "8")}
+          className="flex justify-center items-center bg-[#4B4B4B] w-14 h-14 md:w-20 md:h-20  p-1.5 text-2xl rounded-full"
+        >
           8
         </button>
 
-        <button className="flex justify-center items-center bg-[#4B4B4B] w-14 h-14 md:w-20 md:h-20  p-1.5 text-2xl rounded-full">
+        <button
+          onClick={() => setScreen(screen + "9")}
+          className="flex justify-center items-center bg-[#4B4B4B] w-14 h-14 md:w-20 md:h-20  p-1.5 text-2xl rounded-full"
+        >
           9
         </button>
 
-        <button className="flex justify-center items-center bg-[#4B4B4B] w-14 h-14 md:w-20 md:h-20  p-1.5 text-2xl rounded-full">
+        <button
+          onClick={() => setScreen(screen + "4")}
+          className="flex justify-center items-center bg-[#4B4B4B] w-14 h-14 md:w-20 md:h-20  p-1.5 text-2xl rounded-full"
+        >
           4
         </button>
 
-        <button className="flex justify-center items-center bg-[#4B4B4B] w-14 h-14 md:w-20 md:h-20  p-1.5 text-2xl rounded-full">
+        <button
+          onClick={() => setScreen(screen + "5")}
+          className="flex justify-center items-center bg-[#4B4B4B] w-14 h-14 md:w-20 md:h-20  p-1.5 text-2xl rounded-full"
+        >
           5
         </button>
 
-        <button className="flex justify-center items-center bg-[#4B4B4B] w-14 h-14 md:w-20 md:h-20  p-1.5 text-2xl rounded-full">
+        <button
+          onClick={() => setScreen(screen + "6")}
+          className="flex justify-center items-center bg-[#4B4B4B] w-14 h-14 md:w-20 md:h-20  p-1.5 text-2xl rounded-full"
+        >
           6
         </button>
 
-        <button className="flex justify-center items-center bg-[#4B4B4B] w-14 h-14 md:w-20 md:h-20  p-1.5 text-2xl rounded-full">
+        <button
+          onClick={() => setScreen(screen + "1")}
+          className="flex justify-center items-center bg-[#4B4B4B] w-14 h-14 md:w-20 md:h-20  p-1.5 text-2xl rounded-full"
+        >
           1
         </button>
 
-        <button className="flex justify-center items-center bg-[#4B4B4B] w-14 h-14 md:w-20 md:h-20  p-1.5 text-2xl rounded-full">
+        <button
+          onClick={() => setScreen(screen + "2")}
+          className="flex justify-center items-center bg-[#4B4B4B] w-14 h-14 md:w-20 md:h-20  p-1.5 text-2xl rounded-full"
+        >
           2
         </button>
 
-        <button className="flex justify-center items-center bg-[#4B4B4B] w-14 h-14 md:w-20 md:h-20  p-1.5 text-2xl rounded-full">
+        <button
+          onClick={() => setScreen(screen + "3")}
+          className="flex justify-center items-center bg-[#4B4B4B] w-14 h-14 md:w-20 md:h-20  p-1.5 text-2xl rounded-full"
+        >
           3
         </button>
       </div>
 
       <div className="mt-2.5 flex gap-2.5 text-white">
-        <button className="flex justify-center items-center rounded-full p-1.5 w-14 h-14 md:w-20 md:h-20  text-4xl font-extrabold bg-[#c77dff]">
+        <button
+          onClick={() => setScreen(screen + "+")}
+          className="flex justify-center items-center rounded-full p-1.5 w-14 h-14 md:w-20 md:h-20  text-4xl font-extrabold bg-[#c77dff]"
+        >
           +
         </button>
 
-        <button className="flex justify-center items-center rounded-full p-1.5 w-14 h-14 md:w-20 md:h-20  text-4xl font-extrabold bg-[#c77dff]">
+        <button
+          onClick={() => setScreen(screen + "-")}
+          className="flex justify-center items-center rounded-full p-1.5 w-14 h-14 md:w-20 md:h-20  text-4xl font-extrabold bg-[#c77dff]"
+        >
           -
         </button>
 
-        <button className="flex justify-center items-center rounded-full p-1.5 w-14 h-14 md:w-20 md:h-20  text-4xl font-extrabold bg-[#c77dff]">
+        <button
+          onClick={() => setScreen(screen + "*")}
+          className="flex justify-center items-center rounded-full p-1.5 w-14 h-14 md:w-20 md:h-20  text-4xl font-extrabold bg-[#c77dff]"
+        >
           x
         </button>
 
-        <button className="flex justify-center items-center rounded-full p-1.5 w-14 h-14 md:w-20 md:h-20  text-4xl font-extrabold bg-[#c77dff]">
+        <button
+          onClick={() => setScreen(screen + "/")}
+          className="flex justify-center items-center rounded-full p-1.5 w-14 h-14 md:w-20 md:h-20  text-4xl font-extrabold bg-[#c77dff]"
+        >
           ÷
         </button>
       </div>
 
-      {/* <button className="absolute top-0 right-2.5 h-64 p-3.5 rounded-full bg-[#c77dff] font-extrabold text-4xl text-white">
-        {" "}
-        ={" "}
-      </button> */}
-
       <div className="absolute flex flex-col gap-2.5 top-0 right-0 h-64 rounded-full text-white font-bold">
-        <button className="text-4xl w-14 h-14 md:w-20 md:h-20  p-1.5 rounded-full bg-[#c77dff]">
+        <button
+          onClick={() => {
+            const result = eval(screen);
+            setCurrentOperation(result);
+            setScreen(result.toString());
+          }}
+          className="text-4xl w-14 h-14 md:w-20 md:h-20  p-1.5 rounded-full bg-[#c77dff]"
+        >
           {" "}
           ={" "}
         </button>
 
-        <button className="text-4xl w-14 h-14 md:w-20 md:h-20  p-1.5 rounded-full bg-[#4B4B4B]">
+        <button
+          onClick={() => setScreen(screen + ".")}
+          className="text-4xl w-14 h-14 md:w-20 md:h-20  p-1.5 rounded-full bg-[#4B4B4B]"
+        >
           ,
         </button>
 
-        <button className="flex justify-center items-center bg-[#4B4B4B] w-14 h-14 md:w-20 md:h-20  p-1.5 text-2xl rounded-full">
+        <button
+          onClick={() => setScreen(screen + "0")}
+          className="flex justify-center items-center bg-[#4B4B4B] w-14 h-14 md:w-20 md:h-20  p-1.5 text-2xl rounded-full"
+        >
           0
         </button>
       </div>
